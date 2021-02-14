9a79204fc5a73ef7c4af768a7dbe5723bdce92cc
Ajey Gadheeya <67706606+gadheeya@users.noreply.github.com>
2021-02-14 11:46:14 +0000

    combined two functions

--- func.py
+++ func.py
@@ -1,28 +1,32 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-
-def encrypt(estr,nshi):
-    sstr = ""
-    for num in range (len(estr)):
-        letter = estr[num]
-        lsn = alphabet.index(letter) + nshi
-        lspos= 0
-        if lsn >= len(alphabet):
-            lspos = (lsn % len(alphabet))
-        elif lsn >= len(alphabet):
-            lspos = (lsn % len(alphabet))
-        else:
-            lspos = lsn
-        sstr += alphabet[lspos]
-    print(sstr)
-
-
-
-def decrypt(sstr,sshi):
-    rstr = ""
-    for num in range (len(sstr)):
-        letter = sstr[num]
-        lsn = alphabet.index(letter) - sshi
+def dyncrypt(direc,dstr,dshi):
+    fstr = ""
+    for num in range (len(dstr)):
+        letter = dstr[num]
+        if direc == "e":
+            work = "encoded"
+            lsn = alphabet.index(letter) + dshi
+        elif direc == "d":
+            lsn = alphabet.index(letter) - dshi
+            work = "decoded"
         lspos= 0
         if lsn < 0:
             lspos = (lsn % len(alphabet))
@@ -30,5 +34,39 @@ def decrypt(sstr,sshi):
             lspos = (lsn % len(alphabet))
         else:
             lspos = lsn
-        rstr += alphabet[lspos]
-    print(rstr)
+        fstr += alphabet[lspos]
+    print(f"\nThe {work} text is \n\n{fstr}\n")
+
+
+
+# def encrypt(estr,nshi):
+#     sstr = ""
+#     for num in range (len(estr)):
+#         letter = estr[num]
+#         lsn = alphabet.index(letter) + nshi
+#         lspos= 0
+#         if lsn >= len(alphabet):
+#             lspos = (lsn % len(alphabet))
+#         elif lsn >= len(alphabet):
+#             lspos = (lsn % len(alphabet))
+#         else:
+#             lspos = lsn
+#         sstr += alphabet[lspos]
+#     print(f"\nThe encoded text is \n\n{sstr}\n")
+
+
+
+# def decrypt(sstr,sshi):
+#     rstr = ""
+#     for num in range (len(sstr)):
+#         letter = sstr[num]
+#         lsn = alphabet.index(letter) - sshi
+#         lspos= 0
+#         if lsn < 0:
+#             lspos = (lsn % len(alphabet))
+#         elif lsn >= len(alphabet):
+#             lspos = (lsn % len(alphabet))
+#         else:
+#             lspos = lsn
+#         rstr += alphabet[lspos]
+#     print(f"\nThe encoded text is \n\n{rstr}\n")
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from func import encrypt , decrypt
+from func import dyncrypt , logo
+
+print (logo)
 
 direction = input("Type 'e' to encrypt, type 'd' to decrypt:\n")
 
-
 if direction == "e":
-    text = input("Type your message to encrypt :\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    encrypt(estr=text,nshi=shift)
-
+    text = input("\nType your message to encrypt :\n").lower()
+    shift = int(input("\nType the shift number : \n"))
+    dyncrypt(direc=direction,dstr=text,dshi=shift)
 elif direction == "d":
-    text = input("Type your message to decrypt :\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    decrypt(sstr=text,sshi=shift)
+    text = input("\nType your message to dycrypt :\n").lower()
+    shift = int(input("\nType the shift number : \n"))
+    dyncrypt(direc=direction,dstr=text,dshi=shift)
 else:
     print(f"you gave wrong direction {direction}")
+
+
+
+
+
+
+
+
+
+# if direction == "e":
+#     text = input("\nType your message to encrypt :\n").lower()
+#     shift = int(input("\nType the shift number : \n"))
+#     encrypt(estr=text,nshi=shift)
+
+# elif direction == "d":
+#     text = input("\nType your message to decrypt :\n").lower()
+#     shift = int(input("\nType the shift number : \n"))
+#     decrypt(sstr=text,sshi=shift)
+# else:
+#     print(f"you gave wrong direction {direction}")
